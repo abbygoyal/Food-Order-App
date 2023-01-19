88842c3600cea9cfaa79225cff461aa04b773ed2
abbygoyal <abhishekgoyal3669@gmail.com>
2023-01-19 17:43:22 +0530

    new component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,38 +1,42 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
-import Input from "../../UI/Input";
-import classes from "./MealItemForm.module.css";
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
   const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
-    const eneterdAmount = amountInputRef.current.value;
-    const eneterdAmountNumber = +eneterdAmount;
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
 
     if (
-      eneterdAmount.trim().length === 0 ||
-      eneterdAmountNumber < 1 ||
-      eneterdAmountNumber > 5
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
     ) {
       setAmountIsValid(false);
       return;
     }
-    props.onAddToCart(eneterdAmountNumber);
+
+    props.onAddToCart(enteredAmountNumber);
   };
+
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
         ref={amountInputRef}
-        label="Amount"
+        label='Amount'
         input={{
-          id: "amount",
-          type: "number",
-          min: "1",
-          max: "5",
-          step: "1",
-          defaultValue: "1",
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
         }}
       />
       <button>+ Add</button>
